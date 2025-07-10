27a48f7f2274f7d1cdd935bcacb58bf053851216
Mike Pastore <mike@oobak.org>
2025-07-10 10:39:48 -0500

    checkpoint flawed overlay work

--- buildroot-external/board/raspberrypi/hassos-hook.sh
+++ buildroot-external/board/raspberrypi/hassos-hook.sh
@@ -26,7 +26,19 @@ function hassos_pre_image() {
         sed -i "s|#arm_64bit|arm_64bit|g" "${BOOT_DATA}/config.txt"
     fi
 
-    echo -e "\n# Enable pwm fan control\ndtoverlay=pwm-2chan" >> "${BOOT_DATA}/config.txt"
+    {
+        echo
+        echo "# Enable pwm fan control"
+        echo "dtoverlay=pwm-2chan"
+
+        echo
+        echo "# Initiate shutdown when button is pressed for two or more seconds"
+        echo "dtoverlay=gpio-shutdown,gpio_pin=4,active_low=0,gpio_pull=down"
+
+        echo
+        echo "# Cut power after system shutdown"
+        echo "dtoverlay=gpio-poweroff,gpiopin=27,active_low=0"
+    } >> "${BOOT_DATA}/config.txt"
 }
 
 
--- buildroot-external/package/xscript/xscript.mk
+++ buildroot-external/package/xscript/xscript.mk
@@ -11,30 +11,40 @@ XSCRIPT_SITE = $(call github,geekworm-com,xscript,$(XSCRIPT_VERSION))
 # in the repository at time of writing.
 XSCRIPT_LICENSE = MIT
 
+XSCRIPT_DEPENDENCIES += bc
 XSCRIPT_DEPENDENCIES += libgpiod
-ifeq ($(filter y,$(BR2_PACKAGE_XSCRIPT_FAN_SERVICE) $(BR2_PACKAGE_XSCRIPT_PWR_SERVICE)),y)
+ifeq ($(filter y,$(BR2_PACKAGE_XSCRIPT_BOOT_SERVICE) $(BR2_PACKAGE_XSCRIPT_FAN_SERVICE) $(BR2_PACKAGE_XSCRIPT_POWER_SERVICE)),y)
 XSCRIPT_DEPENDENCIES += systemd
 endif
 
 define XSCRIPT_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/x-c1-fan.sh \
-		$(TARGET_DIR)/usr/local/bin/x-c1-fan.sh
+	$(INSTALL) -D -m 0755 $(@D)/x-c1-fan.sh $(TARGET_DIR)/usr/local/bin/x-c1-fan.sh
+	$(INSTALL) -D -m 0755 $(@D)/xPWR.sh $(TARGET_DIR)/usr/local/bin/xPWR.sh
+	$(INSTALL) -D -m 0755 $(@D)/xSoft.sh $(TARGET_DIR)/usr/local/bin/xSoft.sh
 
-	$(INSTALL) -D -m 0755 $(@D)/xPWR.sh \
-		$(TARGET_DIR)/usr/local/bin/xPWR.sh
+	$(INSTALL) -D -m 0644 $(XSCRIPT_PKGDIR)/99-gpio-power-switch.rules $(TARGET_DIR)/lib/udev/rules.d/
 endef
 
 define XSCRIPT_INSTALL_INIT_SYSTEMD
-	# Conditionally install the fan helper & unit
+	if [ "$(BR2_PACKAGE_XSCRIPT_BOOT_SERVICE)" = "y" ]; then \
+		$(INSTALL) -D -m 0644 $(@D)/x-c1-boot.service \
+			$(TARGET_DIR)/usr/lib/systemd/system/x-c1-boot.service; \
+	else \
+		rm -f $(TARGET_DIR)/usr/lib/systemd/system/x-c1-boot.service; \
+	fi
+
 	if [ "$(BR2_PACKAGE_XSCRIPT_FAN_SERVICE)" = "y" ]; then \
 		$(INSTALL) -D -m 0644 $(@D)/x-c1-fan.service \
 			$(TARGET_DIR)/usr/lib/systemd/system/x-c1-fan.service; \
+	else \
+		rm -f $(TARGET_DIR)/usr/lib/systemd/system/x-c1-fan.service; \
 	fi
 
-	# Conditionally install the power helper & unit
-	if [ "$(BR2_PACKAGE_XSCRIPT_PWR_SERVICE)" = "y" ]; then \
+	if [ "$(BR2_PACKAGE_XSCRIPT_POWER_SERVICE)" = "y" ]; then \
 		$(INSTALL) -D -m 0644 $(@D)/x-c1-pwr.service \
 			$(TARGET_DIR)/usr/lib/systemd/system/x-c1-pwr.service; \
+	else \
+		rm -f $(TARGET_DIR)/usr/lib/systemd/system/x-c1-pwr.service; \
 	fi
 endef
 
